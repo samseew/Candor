f1d1901c59b98b459f384e9725f005b8fa32b26b
samashtar <sam.ashtar@flatironschool.com>
2018-11-26 18:18:00 -0500

    set up api

--- app/components/LoggedIn/Containers/ItemContainer.js
+++ app/components/LoggedIn/Containers/ItemContainer.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 import Item from "../Presentational/Item";
-import { Icon } from "native-base";
+import { Icon, Container, Content, Text } from "native-base";
+
+const Key = "OvTrWbUT";
+
+//  example Api Call - query, location (latitude, longitude), results per page and page number
+// https://api.discountapi.com/v2/deals?query=adult&location=39.151958,-84.477405&per_page=30&page=1
+
 export default class ItemContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      isLoading: false
+    });
+  }
   render() {
-    return (
-      <View style={{ flex: 1, backgroundColor: "orange" }}>
-        <Item />
-      </View>
-    );
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      );
+    } else {
+      return (
+        <Container>
+          <Content>
+            <Text>lmao items</Text>
+            <Item />
+          </Content>
+        </Container>
+      );
+    }
   }
 }
 
