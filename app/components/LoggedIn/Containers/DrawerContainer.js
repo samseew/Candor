c0a2c512fd7a86d8a1ea97218e3b7c4337d073fb
samashtar <sam.ashtar@flatironschool.com>
2018-11-26 16:29:31 -0500

    search styling

--- app/components/LoggedIn/Containers/DrawerContainer.js
+++ app/components/LoggedIn/Containers/DrawerContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 // import { connect } from "react-redux";
 // import { fetchData } from "../actions/asyncAction.js";
-import { View, Text, Button, TouchableOpacity, StyleSheet } from "react-native";
-import { Icon } from "native-base";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Icon, Button } from "native-base";
 import Logo from "../../../components/LoggedOut/Logo.js";
 
 class DrawerContainer extends Component {
@@ -19,7 +19,7 @@ class DrawerContainer extends Component {
           <TouchableOpacity
             onPress={() => this.props.navigation.navigate("Drawer")}
           >
-            <Icon name="settings" />
+            <Icon name="menu" />
           </TouchableOpacity>
         </View>
         <View style={styles.TitleBox}>
@@ -27,6 +27,9 @@ class DrawerContainer extends Component {
             <Logo />
           </View>
         </View>
+        <Button transparent onPress={() => console.log("lmao it wokred")}>
+          <Icon name="search" />
+        </Button>
       </View>
     );
   }
@@ -36,13 +39,15 @@ export default DrawerContainer;
 
 const styles = StyleSheet.create({
   Icon: {
-    flex: 2
+    flex: 2,
+    marginLeft: 10,
+    marginTop: 10
   },
   TitleBox: {
     backgroundColor: "white",
     flex: 9,
     alignItems: "center",
-    right: "30%"
+    right: "40%"
   },
   Logo: {
     backgroundColor: "green"
